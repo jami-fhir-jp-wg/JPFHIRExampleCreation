8a7fdfb850ad8ebe688e1fdaac2482fe8f98ec61
kazuhiko-ohe <kohe@m.u-tokyo.ac.jp>
2022-12-19 06:24:27 +0900

    フォルダ再編成に対応

--- reload_eReferral.sh
+++ reload_eReferral.sh
@@ -1,12 +1,13 @@
-targetProject=eReferral
 targetPackage=jp-ereferral#0.9.3-snap.tgz
-targetFile=Bundle-bundleReferralExample01.json
+targetProject=eReferral
+targetExample=example01
+validateFile=Bundle-bundleReferralExample01.json
 #
 cd ~/GitHub/JPFHIRExampleCreation
 rm -f ./input/fsh/$targetProject/*.fsh*
 while read -r f
     do outfname=`basename $f .fshegg`
-    echo "converting $f to ./input/fsh/$targetProject/$outfname.fsh"
+    echo "converting $f to ./input/fsh/$targetProject/$targetExample/$outfname.fsh"
     filepp -b  -I./fshegg \
       -M/usr/local/share/filepp/modules  \
       -m bigdef.pm  \
@@ -15,17 +16,17 @@ while read -r f
       -M ./perlFunc   \
       -m base64.pm  \
       -m uuid4.pm  \
-      $f >./input/fsh/$targetProject/$outfname.fsh
-done < <(find ./fshegg/$targetProject -name *.fshegg )
+      $f >./input/fsh/$targetProject/$targetExample/$outfname.fsh
+done < <(find ./fshegg/$targetProject/$targetExample -name *.fshegg )
 # Conpositionの相対URL参照をBundle内のFullURL参照に書き換える
 python3 pyscripts/addFullUrl2Composition.py  $targetProject
-mkdir -p ./input/fsh/$targetProject/backup
-mv ./input/fsh/$targetProject/*.fsh_backup ./input/fsh/$targetProject/backup/
-sushi .  -o ./output-json/$targetProject
+mkdir -p ./input/fsh/$targetProject/$targetExample/backup
+mv ./input/fsh/$targetProject/$targetExample/*.fsh_backup ./input/fsh/$targetProject/$targetExample/backup/
+sushi .  -o ./output-json/$targetProject/$targetExample
 cd ~/GitHub/
 java -jar work/validator_cli.jar \
-    JPFHIRExampleCreation/output-json/$targetProject/fsh-generated/resources/$targetFile \
+    JPFHIRExampleCreation/output-json/$targetProject/$targetExample/fsh-generated/resources/$targetFile \
     -ig JPFHIRExampleCreation/packages_snapshot/$targetPackage \
     -tx https://tx.jpfhir.jp:8081 \
-    >JPFHIRExampleCreation/output.txt
+    >JPFHIRExampleCreation/$targetProject/$targetExample/output.txt
 cd ~/GitHub/JPFHIRExampleCreation
